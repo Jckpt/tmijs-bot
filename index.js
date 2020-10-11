1b63eb4cb9a4bbf2e21f6f329a1d31536afc81d3
Jckpt <64931967+Jckpt@users.noreply.github.com>
2020-10-11 16:39:03 +0200

    added subscribe check

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const tmi = require('tmi.js');
 const mysql = require('mysql');
-//const cleverbot = require('cleverbot-free');
 require('dotenv').config();
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'nodemysql',
+  database: 'twitchbot',
 });
 db.connect((err) => {
   if (err) {
@@ -14,15 +13,9 @@ db.connect((err) => {
   }
   console.log('Mysql connected...');
 });
-// let query = 'CREATE TABLE users(id int AUTO_INCREMENT, nick varchar(30), subscribed_to varchar(30), gifted_by varchar(30), created_on DATE, last_updated DATE, PRIMARY KEY (id));';
-// db.query(query, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('created table');
-// });
-// var messages = [];
 const stinkers = ['overpow', 'randombrucetv'];
 const beOnChat = ['lukisteve', 'krawcu_'];
+var everyChannel = [...stinkers, ...beOnChat];
 var stallTheCommand = false;
 const onSub = (channel, username, gifter, gifted) => {
   channel = channel.substring(1);
@@ -31,13 +24,13 @@ const onSub = (channel, username, gifter, gifted) => {
     console.log(`checking if ${username} is already in database`);
     if (err) throw err;
     if (result.length > 0) {
-      let query = `UPDATE users SET last_updated=now(), gifted_by="${gifter}" WHERE nick="${username}" AND subscribed_to="${channel}";`;
+      let query = `UPDATE users SET last_updated=now(), gifted_by="${gifter}", is_expired="F" WHERE nick="${username}" AND subscribed_to="${channel}";`;
       db.query(query, (err, result) => {
         if (err) throw err;
         console.log(result);
       });
     } else {
-      let query = 'INSERT INTO users SET ?, created_on=now(), last_updated=now()';
+      let query = 'INSERT INTO users SET ?, last_updated=now(), is_expired="F"';
       let user = { nick: username, subscribed_to: channel, gifted_by: gifter };
       db.query(query, user, (err, result) => {
         if (err) throw err;
@@ -49,18 +42,13 @@ const onSub = (channel, username, gifter, gifted) => {
   if (stinkers.includes(channel)) {
     beOnChat.forEach((chat) => {
       if (gifted) {
-        let reason = `Sub gift u ${channel} 🧀`;
+        let reason = `Sub gift u ${channel} dla ${username} 🧀`;
         client.ban(chat, gifter, reason).catch((err) => {
-          client.say(chat, `${username} dostał subgift od ${gifter} na kanale ${channel} StinkyCheese`).catch((err) => {
-            console.log(err);
-          });
           console.log(err);
         });
       } else {
+        let reason = `Sub u ${channel} 🧀`;
         client.ban(chat, username, reason).catch((err) => {
-          client.say(chat, `${username} zasubskrybował kanał ${channel} StinkyCheese`).catch((err) => {
-            console.log(err);
-          });
           console.log(err);
         });
       }
@@ -77,7 +65,7 @@ const client = new tmi.Client({
     username: process.env.BOT_USERNAME,
     password: `oauth:${process.env.OAUTH_TOKEN}`,
   },
-  channels: [...stinkers, ...beOnChat],
+  channels: everyChannel,
 });
 client.connect();
 client.on('subscription', (channel, username) => {
@@ -90,49 +78,32 @@ client.on('subgift', (channel, username, streakMonths, recipient, methods, users
   onSub(channel, recipient, username, true);
 });
 client.on('message', (channel, tags, message, self) => {
-  channel = channel.substring(1);
+  if (self || stinkers.includes(channel) || !message.startsWith('$')) return;
   let delay = 5000;
-  if (self) return;
-  if (stinkers.includes(channel)) return;
-  if (
-    message.toLowerCase() === `@${process.env.BOT_USERNAME} pogchamp` ||
-    message.toLowerCase() === `@${process.env.BOT_USERNAME}, pogchamp` ||
-    message.toLowerCase() === `${process.env.BOT_USERNAME}, pogchamp` ||
-    message.toLowerCase() === `${process.env.BOT_USERNAME} pogchamp`
-  ) {
-    if (stallTheCommand === true) return;
-    client.say(channel, `@${tags.username} PogChamp`);
-    stallTheCommand = true;
-    setTimeout(() => {
-      stallTheCommand = false;
-    }, delay);
+  const args = message.slice(1).split(' ');
+  const command = args.shift().toLowerCase();
+  let username = args[0];
+  let targetChannel = args[1];
+  if (command === 'sub' && !stallTheCommand) {
+    console.log(`username: ${username}, targetChannel: ${targetChannel}`);
+    if (everyChannel.includes(`#${targetChannel}`)) {
+      let query = `SELECT nick, subscribed_to, is_expired FROM users WHERE nick="${username}" AND subscribed_to="${targetChannel}" AND is_expired="F";`;
+      db.query(query, (err, result) => {
+        console.log(`checking if ${username} is already in database`);
+        if (err) throw err;
+        if (result.length > 0) {
+          console.log(result);
+          client.say(channel, `@${tags.username}, ten użytkownik ma suba na tam tym kanale.`);
+        } else {
+          client.say(channel, `@${tags.username}, ten użytkownik nie ma suba na tam tym kanale.`);
+        }
+      });
+    } else {
+      client.say(channel, `@${tags.username}, nie mam zapisanych subskrybentów tego kanału.`);
+    }
   }
-  //   if (
-  //     message.toLowerCase() === `@${process.env.BOT_USERNAME} vislaud` ||
-  //     message.toLowerCase() === `@${process.env.BOT_USERNAME}, vislaud` ||
-  //     message.toLowerCase() === `${process.env.BOT_USERNAME}, vislaud` ||
-  //     message.toLowerCase() === `${process.env.BOT_USERNAME} vislaud`
-  //   ) {
-  //     if (stallTheCommand === true) return;
-  //     client.say(channel, `@${tags.username} VisLaud`);
-  //     stallTheCommand = true;
-  //     setTimeout(() => {
-  //       stallTheCommand = false;
-  //     }, delay);
-  //   }
-  //   if (channel === 'lukisteve' && message.startsWith('$m')) {
-  //     if (stallTheCommand === true) return;
-  //     message = message.substring(3);
-  //     cleverbot(message, messages).then((response) => {
-  //       messages = [...messages, message];
-  //       if (messages.length >= 11) {
-  //         messages.shift();
-  //       }
-  //       client.say(channel, `@${tags.username} ${response}`);
-  //     });
-  //   }
-  //   stallTheCommand = true;
-  //   setTimeout(() => {
-  //     stallTheCommand = false;
-  //   }, delay);
+  stallTheCommand = true;
+  setTimeout(() => {
+    stallTheCommand = false;
+  }, delay);
 });
